6a90e2a04eb0a22fe0d0254c842dac8a2245591f
Mike Mitterer <office@mikemitterer.at>
2021-10-05 17:38:15 +0200

    Version 2

--- hello_world/_version.py
+++ hello_world/_version.py
@@ -12,7 +12,7 @@ version_json = '''
  "dirty": false,
  "error": null,
  "full-revisionid": "9000ba0ddba0f96f88e4e952d62656a8bb18d8c3",
- "version": "0.0.1"
+ "version": "0.0.2"
 }
 '''  # END VERSION_JSON
 
